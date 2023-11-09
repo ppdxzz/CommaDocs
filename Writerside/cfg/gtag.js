2731210dbf4eed6a4d8c09dd3f93b22df4acfb8c
peision <peichenwan@gmail.com>
2023-11-09 16:34:46 +0800

    feat(buildprofiles.xml): 新增谷歌分析

---
+++ Writerside/cfg/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQZL6NB2TK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HQZL6NB2TK');
+</script>
